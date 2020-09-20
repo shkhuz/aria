deb0e38d6807b8d2327366a9f18f353d33ac126b
huzaifash <huzaifash@tuta.io>
2020-09-20 18:37:26 +0530

    Restart Aria Project

--- src/include/ast_print.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AST_PRINT_H
-#define _AST_PRINT_H
-
-#include <stmt.h>
-
-void print_ast(Stmt** stmts);
-
-#endif /* _AST_PRINT_H */
--- src/include/keywords.h
+++
@@ -1,8 +0,0 @@
-#ifndef _KEYWORDS_H
-#define _KEYWORDS_H
-
-void keywords_init(void);
-
-extern const char** keywords;
-
-#endif /* _KEYWORDS_H */
