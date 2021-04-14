cd2c9ff1fc70bfde55a9d83ff3af8cb22873c158
Huzaifa Shaikh <huzaifash@tuta.io>
2021-04-15 00:19:34 +0530

    Lex: boilerplate

--- src/aria.h
+++ src/aria.h
@@ -6,4 +6,11 @@ typedef struct {
 	File* contents;
 } SrcFile;
 
+///// LEXER /////
+typedef struct {
+	SrcFile* srcfile;
+} Lexer;
+
+void lexer_init(Lexer* self, SrcFile* srcfile);
+
 #endif	/* __ARIA_H */
